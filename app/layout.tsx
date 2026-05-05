3d2a96157a8896bb8adc6b3371d0b4eb3ef8726c
xiaogang.li <xiaogang.li@cloudsky.com>
2026-05-05 09:50:04 +0800

    修改网页标题，图标

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,14 @@ import { NextIntlClientProvider } from 'next-intl'
 import { getLocale, getMessages } from 'next-intl/server'
 
 export const metadata: Metadata = {
-  title: 'sensor frontend',
+  title: 'WatchPoint',
   description: 'endpoint security',
   generator: 'lxg',
+  icons: {
+    icon: '/logo.svg',
+    shortcut: '/logo.svg',
+    apple: '/logo.svg',
+  },
 }
 
 export default async function RootLayout({children,}: Readonly<{children: React.ReactNode}>) {
