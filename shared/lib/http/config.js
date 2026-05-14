60bcb3eb9ec0a617c998321ad8b8df72ed31ef8f
lxg <wxi666@126.com>
2026-05-15 01:37:19 +0800

    1

--- shared/lib/http/config.js
+++ shared/lib/http/config.js
@@ -62,6 +62,7 @@ const FALLBACK_CONFIG = {
       getHostPatchSummary: "/sensor/patch/host/summary",
       getHostPatches: "/sensor/patch/host/list",
       getAllPatchCoverageForInstall: "/sensor/patch/patches/coverage-all",
+      createPatchInstallTask: "/sensor/control/patch/installtask",
       applyPMCObject: "/sensor/control/applypmcobject",
       patchImmediateScan: "/sensor/control/patch/immediatescan",
       patchOneClickRepair: "/sensor/control/patch/oneclickrepair",
