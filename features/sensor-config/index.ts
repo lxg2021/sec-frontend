5a4d85e483d541515f3f645bde943104fdf1bd70
lxg <wxi666@126.com>
2026-07-27 18:02:42 +0800

    chore: remove unused frontend source files

--- features/sensor-config/index.ts
+++
@@ -1,4 +0,0 @@
-// 统一导出配置组件
-export { ConfigList } from "./config-list"
-export { ConfigCreateDialog } from "./config-create-dialog"
-export { ConfigTable } from "./config-table"
--- shared/lib/response-parser.js
+++
@@ -1,6 +0,0 @@
-import { parseApiResponse } from "@/shared/lib/http/response"
-
-export function parseResponse(responseBody) {
-  return parseApiResponse(responseBody)
-}
-
